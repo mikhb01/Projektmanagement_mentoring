92fe0b7deacd14b2e7ef31e017373d2bfcfa65f8
mikhb01 <lengertleon@gmail.com>
2026-01-08 11:40:41 +0100

    - udated function for mentor clicking

---
+++ JS/mentorClick.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    // const mentor1 = document.getElementById('mentor1');
+    // const mentor2 = document.getElementById('mentor2');
+    // const mentor3 = document.getElementById('mentor3');
+
+    // if (mentor1) {
+    //     mentor1.addEventListener('click', () => {
+    //         // alert("Du hast Sarah Schmidt ausgewählt!");
+    //         window.location.href = "../sites/mentors/mentorSchmidtS.html";
+    //     });
+    // }
+
+    // if (mentor2) {
+    //     mentor2.addEventListener('click', () => {
+    //         alert("Du hast Michael Müller ausgewählt!");
+    //     });
+    // }
+
+    // if (mentor3) {
+    //     mentor3.addEventListener('click', () => {
+    //         alert("Du hast Julia Weber ausgewählt!");
+    //     });
+    // }
+
+    document.addEventListener('click', (event) => {
+        const mentorCard = event.target.closest('.box');
+        if (mentorCard) {
+            const mentorId = mentorCard.id;
+            if (mentorId === 'mentor1') {
+                window.location.href = "../sites/mentors/mentorSchmidtS.html";
+            } else if (mentorId === 'mentor2') {
+                alert("Du hast Michael Müller ausgewählt!");
+            } else if (mentorId === 'mentor3') {
+                alert("Du hast Julia Weber ausgewählt!");
+            }
+        }
+    });
+});
